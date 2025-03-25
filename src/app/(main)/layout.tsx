e298fdaf7d54d2cd7157ed4aeeb36b1a7fdeb47a
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-03-25 16:42:59 +0100

    minimal repro

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import { TRPCReactProvider } from "@/trpc/react";
 import { Layout } from "../_components/layout";
 import type { ReactNode } from "react";
+import { HydrateClient } from "@/trpc/server";
 
 // const geist = Geist({
 //   subsets: ["latin"],
@@ -12,7 +13,9 @@ export default function RootLayout({
 }: Readonly<{ children: ReactNode }>) {
   return (
     <TRPCReactProvider>
-      <Layout>{children}</Layout>
+      <HydrateClient>
+        <Layout>{children}</Layout>
+      </HydrateClient>
     </TRPCReactProvider>
   );
 }
