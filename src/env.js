c378b86fe8b2954353e3a9b50da0d16e90bb388a
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-03-25 16:50:12 +0100

    add missing env variables to env.js and .env.example

--- src/env.js
+++ src/env.js
@@ -7,16 +7,18 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
+    NODE_ENV: z
+      .enum(['development', 'test', 'production'])
+      .default('development'),
+    DATABASE_URL: z.string().url(),
     AUTH_SECRET:
       process.env.NODE_ENV === 'production'
         ? z.string()
         : z.string().optional(),
     AUTH_GOOGLE_CLIENT_ID: z.string(),
     AUTH_GOOGLE_CLIENT_SECRET: z.string(),
-    DATABASE_URL: z.string().url(),
-    NODE_ENV: z
-      .enum(['development', 'test', 'production'])
-      .default('development'),
+    AUTH_TRUST_HOST: z.literal('TRUE'),
+    AUTH_URL: z.string().url(),
   },
 
   /**
@@ -35,11 +37,13 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
+    NODE_ENV: process.env.NODE_ENV,
+    DATABASE_URL: process.env.DATABASE_URL,
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_GOOGLE_CLIENT_ID: process.env.AUTH_GOOGLE_CLIENT_ID,
     AUTH_GOOGLE_CLIENT_SECRET: process.env.AUTH_GOOGLE_CLIENT_SECRET,
-    DATABASE_URL: process.env.DATABASE_URL,
-    NODE_ENV: process.env.NODE_ENV,
+    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
+    AUTH_URL: process.env.AUTH_URL,
     NEXT_PUBLIC_NODE_ENV: process.env.NODE_ENV,
   },
   /**
