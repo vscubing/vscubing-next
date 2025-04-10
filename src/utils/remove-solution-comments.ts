9786bb0dd91ed258f829bd52e82da558eafdf326
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-10 08:41:02 +0200

    rename reconstruction to solution

--- src/utils/remove-solution-comments.ts
+++ src/utils/remove-solution-comments.ts
@@ -1,16 +1,16 @@
 import { env } from '@/env'
 
-export function removeSolutionComments(moves: string): string {
-  if (moves.includes('  ') && env.NEXT_PUBLIC_APP_ENV !== 'production') {
+export function removeSolutionComments(solution: string): string {
+  if (solution.includes('  ') && env.NEXT_PUBLIC_APP_ENV !== 'production') {
     alert(
-      '[TWISTY] double space in reconstruction with timestamps, see console error',
+      '[TWISTY] double space in solution with timestamps, see console error',
     )
     throw new Error(
-      `[TWISTY] double space in reconstruction with timestamps, moves: ${moves}`,
+      `[TWISTY] double space in solution with timestamps, moves: ${solution}`,
     )
   }
 
-  return moves
+  return solution
     .replace(/\/\*\d+?\*\//g, '')
     .trim()
     .replaceAll('  ', ' ') // TODO: we shouldn't need this, investigate discrepancies between cstimer and phpless-cstimer
