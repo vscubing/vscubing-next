40fab6eaceedcf011ec776ca49e92b8cc1a87a78
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-10 21:10:42 +0200

    fix serial_sequence in legacy-import migration

--- drizzle/0001_legacy-import.sql
+++ drizzle/0001_legacy-import.sql
@@ -115,9 +115,9 @@ FROM
 
 -- Custom SQL migration file, put your code below! --
 SELECT SETVAL((SELECT PG_GET_SERIAL_SEQUENCE('"scramble"', 'id')), (SELECT (MAX("id") + 1) FROM "scramble"), FALSE);
--- Custom SQL migration file, put your code below! --
 SELECT SETVAL((SELECT PG_GET_SERIAL_SEQUENCE('"round"', 'id')), (SELECT (MAX("id") + 1) FROM "round"), FALSE);
--- Custom SQL migration file, put your code below! --
+SELECT SETVAL((SELECT PG_GET_SERIAL_SEQUENCE('"round_session"', 'id')), (SELECT (MAX("id") + 1) FROM "round_session"), FALSE);
+SELECT SETVAL((SELECT PG_GET_SERIAL_SEQUENCE('"solve"', 'id')), (SELECT (MAX("id") + 1) FROM "solve"), FALSE);
 
 INSERT INTO 
 	user_simulator_settings (created_at, updated_at, user_id, animation_duration, inspection_voice_alert)
