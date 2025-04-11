0badbb2020a1f8830a179136ce9f4edc0282c852
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-11 17:09:38 +0200

    infra: properly disable posthog in local and staging

--- src/backend/posthog.ts
+++ src/backend/posthog.ts
@@ -5,4 +5,5 @@ export const posthogClient = new PostHog(env.NEXT_PUBLIC_POSTHOG_KEY, {
   host: 'https://eu.i.posthog.com',
   flushAt: 1,
   flushInterval: 0,
+  disabled: env.NEXT_PUBLIC_POSTHOG_KEY === 'DISABLED',
 })
