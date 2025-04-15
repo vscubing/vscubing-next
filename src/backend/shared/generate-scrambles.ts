477ebe649621220fbd70508c89f9a1674d897914
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-15 19:18:05 +0200

    fix(app): use proper new URL().searchParams.append to escape special symbols in tnoodle requests

--- src/backend/shared/generate-scrambles.ts
+++ src/backend/shared/generate-scrambles.ts
@@ -3,10 +3,15 @@ import type { Discipline } from '@/types'
 import { z } from 'zod'
 
 export async function generateScrambles(discipline: Discipline, count: number) {
-  if (!env.TNOODLE_URL) throw new Error('TNOODLE_URL missing in .env')
-  return await fetch(
-    `${env.TNOODLE_URL}?discipline=${discipline}&count=${count}&secret=${env.TNOODLE_SECRET}`,
-  )
+  if (!env.TNOODLE_URL || !env.TNOODLE_SECRET)
+    throw new Error('TNOODLE_URL or TNOODLE_SECRET missing in .env')
+
+  const url = new URL(env.TNOODLE_URL)
+  url.searchParams.append('discipline', discipline)
+  url.searchParams.append('count', String(count))
+  url.searchParams.append('secret', env.TNOODLE_SECRET) // can't just do string interpolation because it doesn't escape things like + unlike url.searchParams.append
+
+  return await fetch(url)
     .then((res) => res.json())
     .then((json) => z.array(z.string()).length(count).parse(json))
 }
