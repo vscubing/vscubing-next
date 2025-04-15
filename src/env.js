b34306362bc0dd4b24d3e999eab9064a1fc61ecc
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-15 18:34:24 +0200

    fix env vars

--- src/env.js
+++ src/env.js
@@ -42,8 +42,11 @@ export const env = createEnv({
     NEXT_PUBLIC_APP_ENV: z.enum(['development', 'staging', 'production']),
     NEXT_PUBLIC_POSTHOG_KEY:
       process.env.NEXT_PUBLIC_APP_ENV === 'production'
-        ? z.string()
-        : z.undefined(),
+        ? z.string().refine((val) => val !== 'DISABLED')
+        : z.literal('DISABLED', {
+            message:
+              'NEXT_PUBLIC_POSTHOG_KEY must contain the key in production or DISABLED otherwise',
+          }),
   },
 
   /**
