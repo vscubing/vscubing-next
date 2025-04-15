0521d24860da6077bd9e2875ee0efe3ecda43f3e
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-15 18:29:31 +0200

    fix env vars

--- src/env.js
+++ src/env.js
@@ -52,7 +52,7 @@ export const env = createEnv({
    */
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
-    GH_DOKPLOY_WEBHOOK_URL: process.env.DOKPLOY_WEBHOOK_URL,
+    GH_DOKPLOY_WEBHOOK_URL: process.env.GH_DOKPLOY_WEBHOOK_URL,
     AUTH_SECRET: process.env.AUTH_SECRET,
     AUTH_GOOGLE_CLIENT_ID: process.env.AUTH_GOOGLE_CLIENT_ID,
     AUTH_GOOGLE_CLIENT_SECRET: process.env.AUTH_GOOGLE_CLIENT_SECRET,
