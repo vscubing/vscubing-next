cee80ef5239253cc6788b1c9919ec29f73e02026
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-16 12:41:25 +0200

    fix(simulator): disable touchcube overscroll

--- src/app/(app)/contests/[contestSlug]/solve/_components/simulator/components/simulator/simulator.lazy.tsx
+++ src/app/(app)/contests/[contestSlug]/solve/_components/simulator/components/simulator/simulator.lazy.tsx
@@ -224,33 +224,6 @@ export default function Simulator({
         href='https://fonts.googleapis.com/css2?family=M+PLUS+1+Code&display=swap'
         rel='stylesheet'
       />
-      <style>
-        {`
-        .touchcube {
-          position: absolute;
-          text-align: center;
-          user-select: none;
-        }
-        .touchcube tr td {
-          width: 33%;
-          height: 33%;
-        }
-        .touchcube.active {
-          background-color: #6666;
-          color: #fffa;
-        }
-        .touchcube.active td.touchto {
-          background-color: #0f0a;
-        }
-        .touchcube.active td.touchfrom {
-          background-color: #f00a;
-        }
-        .touchcube.board td {
-          border: 2px solid #6666;
-          border: 0.15rem solid #6666;
-        }
-        `}
-      </style>
       <div
         className='relative flex h-full items-center justify-center'
         onTouchStart={touchStartHandler}
