b7706886c23b389fa4d35af1789048ee6a37f339
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-17 15:25:54 +0200

    Revert Sentry
    This reverts commit 1382c836b22cbcb5358f0086bc9c748470945ef9.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import { withSentryConfig } from '@sentry/nextjs'
 import './src/env.js'
 
 // NOTE: resourceQueries in rules don't work in turbopack yet :(
@@ -48,28 +47,4 @@ const config = {
   output: 'standalone',
 }
 
-export default withSentryConfig(config, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-  org: 'vscubing',
-  project: 'vscubing-next',
-
-  // Only print logs for uploading source maps in CI
-  silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
-  widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  tunnelRoute: '/monitoring',
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
-})
+export default config
