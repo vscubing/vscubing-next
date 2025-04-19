73598f73dcdf1a353ec0b77e39b7c4246e1ba49e
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-19 19:56:34 +0200

    fix(ui): remove developer badge glowing

--- src/frontend/shared/wca-badge-link.tsx
+++ src/frontend/shared/wca-badge-link.tsx
@@ -29,9 +29,9 @@ function WcaBadgeLink({ wcaId }: { wcaId: string }) {
 
 function RecordHolderBadge({ records }: { records: number }) {
   return (
-    <span className='relative -mt-1 inline-flex h-5 w-5 text-amber-400'>
+    <span className='text-amber-400 relative -mt-1 inline-flex h-5 w-5'>
       <TrophyIcon color='currentColor' />
-      <span className='absolute -right-1 top-[-0.2rem] flex h-[14px] min-w-[14px] items-center justify-center rounded-full bg-amber-400 px-[2px] font-kanit text-[12px] font-medium text-grey-80'>
+      <span className='bg-amber-400 absolute -right-1 top-[-0.2rem] flex h-[14px] min-w-[14px] items-center justify-center rounded-full px-[2px] font-kanit text-[12px] font-medium text-grey-80'>
         {records}
       </span>
     </span>
@@ -41,7 +41,7 @@ function RecordHolderBadge({ records }: { records: number }) {
 function DeveloperBadge() {
   return (
     <HoverPopover content='vscubing developer' asChild>
-      <span className='inline-flex h-5 w-5 items-center justify-center gap-0.5 rounded-md border border-primary-60/20 bg-primary-60/10 p-0 text-xs font-semibold text-primary-60 drop-shadow-[0_0_3px_rgba(52,211,153,0.7)] transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2'>
+      <span className='inline-flex h-5 w-5 items-center justify-center gap-0.5 rounded-md border border-primary-60/20 bg-primary-60/10 p-0 text-xs font-semibold text-primary-60 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2'>
         <CodeXmlIcon />
       </span>
     </HoverPopover>
