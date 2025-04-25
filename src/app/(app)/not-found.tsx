7ec93d418b0b4f7e81991f56c1f5b7d472c07ff3
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-04-25 13:24:26 +0200

    fix: not-found for /(app)/

--- src/app/(app)/not-found.tsx
+++ src/app/(app)/not-found.tsx
@@ -2,16 +2,13 @@ import {
   NotFoundContent,
   NotFoundWrapper,
 } from '@/frontend/shared/not-found-content'
-import Layout from './(app)/layout'
 
 export const dynamic = 'force-static'
 
 export default function NotFound() {
   return (
     <NotFoundWrapper>
-      <Layout withoutHeader>
-        <NotFoundContent />
-      </Layout>
+      <NotFoundContent />
     </NotFoundWrapper>
   )
 }
