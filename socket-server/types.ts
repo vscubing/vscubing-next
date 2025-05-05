b8ab76869f4a94986e092bfb757121876dbdc630
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-05-05 19:06:10 +0200

    bump vscubing/cubing for ExperimentalBinary3x3x3Pattern

--- socket-server/types.ts
+++ socket-server/types.ts
@@ -1,7 +1,8 @@
 import type { Move } from '@/types'
+import type { ExperimentalBinary3x3x3Pattern } from '@vscubing/cubing/protocol'
 
 export interface ServerToClientEvents {
-  pattern: (binaryPattern: Uint8Array) => void
+  pattern: (binaryPattern: ExperimentalBinary3x3x3Pattern) => void
   history: (history: string) => void
   onMove: (move: Move) => void
 }
