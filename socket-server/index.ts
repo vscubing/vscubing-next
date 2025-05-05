59bfc8488096a982994d79d4d191ffb3614e18a1
Bohdan Chornokondratenko <bogdan.chornocondratenko@gmail.com>
2025-05-05 10:29:19 +0200

    fix socket-server cors

--- socket-server/index.ts
+++ socket-server/index.ts
@@ -6,7 +6,9 @@ import { puzzles } from '@vscubing/cubing/puzzles'
 // NOTE: bun --watch socket-server/index.ts
 
 const httpServer = createServer()
-const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer)
+const io = new Server<ClientToServerEvents, ServerToClientEvents>(httpServer, {
+  cors: { origin: 'http://localhost:3000' },
+})
 
 const puzzle = await puzzles['3x3x3']!.kpuzzle()
 let pattern = puzzle.defaultPattern()
